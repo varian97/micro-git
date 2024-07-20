e7ef2fe4e1242668db9a5b423e89f6227cbc18ff
varian97 <variancaesar@gmail.com>
2024-07-20 21:58:33 +0700

    add hash-object command

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -1,6 +1,8 @@
 package filesystem
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -8,47 +10,94 @@ import (
 
 const (
 	MICROGIT_FOLDER_NAME = ".microgit"
+
+	BLOB_OBJECT_TYPE   = "blob"
+	TAG_OBJECT_TYPE    = "tag"
+	COMMIT_OBJECT_TYPE = "commit"
+	TREE_OBJECT_TYPE   = "tree"
 )
 
-func Init() {
+func Init() error {
 	err := os.Mkdir(MICROGIT_FOLDER_NAME, 0o774)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize .microgit folder: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
 
 	refsFolderPath := filepath.Join(MICROGIT_FOLDER_NAME, "refs")
 	err = os.Mkdir(refsFolderPath, 0o774)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize refs folder: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
 
 	refsHeadsFolderPath := filepath.Join(MICROGIT_FOLDER_NAME, "refs", "heads")
 	err = os.Mkdir(refsHeadsFolderPath, 0o774)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize refs folder: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
 
 	refsTagsFolderPath := filepath.Join(MICROGIT_FOLDER_NAME, "refs", "tags")
 	err = os.Mkdir(refsTagsFolderPath, 0o774)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize refs folder: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
 
 	objectsFolderPath := filepath.Join(MICROGIT_FOLDER_NAME, "objects")
 	err = os.Mkdir(objectsFolderPath, 0o774)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize objects folder: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
 
 	headsFilePath := filepath.Join(MICROGIT_FOLDER_NAME, "HEAD")
 	err = os.WriteFile(headsFilePath, []byte("ref: refs/heads/master"), 0o664)
 	if err != nil {
 		errorMessage := fmt.Errorf("failed to initialize HEAD: %v", err)
-		panic(errorMessage)
+		return errorMessage
 	}
+
+	return nil
+}
+
+func WriteObject(path, objectType string, shouldWrite bool) (string, error) {
+	if objectType != BLOB_OBJECT_TYPE && objectType != TAG_OBJECT_TYPE && objectType != COMMIT_OBJECT_TYPE && objectType != TREE_OBJECT_TYPE {
+		err := fmt.Errorf("invalid objectType supplied: %v", objectType)
+		return "", err
+	}
+
+	fileContent, err := os.ReadFile(path)
+	if err != nil {
+		err := fmt.Errorf("failed to read file content, %v", err)
+		return "", err
+	}
+
+	combined := append([]byte(objectType), '\x00')
+	combined = append(combined, fileContent...)
+	sha1Sum := sha1.Sum(combined)
+	hexSum := hex.EncodeToString(sha1Sum[:])
+
+	if !shouldWrite {
+		return hexSum, nil
+	}
+
+	initial, fileId := hexSum[:2], hexSum[2:]
+	folderName := filepath.Join(MICROGIT_FOLDER_NAME, "objects", initial)
+	fileName := filepath.Join(folderName, fileId)
+
+	err = os.MkdirAll(folderName, 0o774)
+	if err != nil {
+		err := fmt.Errorf("failed to create object folder, %v", err)
+		return "", err
+	}
+
+	err = os.WriteFile(fileName, combined, 0o664)
+	if err != nil {
+		err := fmt.Errorf("failed to create the object file, %v", err)
+		return "", err
+	}
+
+	return hexSum, nil
 }
--- main.go
+++ main.go
@@ -14,6 +14,20 @@ func main() {
 
 	initCommand := parser.NewCommand("init", "Create an empty micro-git repository or reinitialize an existing one")
 
+	hashObjectCommand := parser.NewCommand("hash-object", "Compute object ID and optionally creates a blob from a file")
+	hashObjectFileType := hashObjectCommand.String("t", "type", &argparse.Options{
+		Help:    "Specify the type (default: \"blob\")",
+		Default: "blob",
+	})
+	hashObjectWriteFlag := hashObjectCommand.Flag("w", "write", &argparse.Options{
+		Required: false,
+		Help:     "Actually write the object into the object database",
+	})
+	hashObjectFileInput := hashObjectCommand.StringPositional(&argparse.Options{
+		Required: true,
+		Help:     "The file to be hashed",
+	})
+
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
@@ -21,6 +35,17 @@ func main() {
 	}
 
 	if initCommand.Happened() {
-		filesystem.Init()
+		err := filesystem.Init()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if hashObjectCommand.Happened() {
+		hexSum, err := filesystem.WriteObject(*hashObjectFileInput, *hashObjectFileType, *hashObjectWriteFlag)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(hexSum)
 	}
 }

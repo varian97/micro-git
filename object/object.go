151e1fb96fc04c89e603dcaf1801f4f79a3b5833
varian97 <variancaesar@gmail.com>
2025-05-25 00:22:51 +0700

    feat: add log command

--- object/object.go
+++ object/object.go
@@ -37,6 +37,14 @@ type treeEntry struct {
 	filename   string
 }
 
+type commitInfo struct {
+	treeOid         string
+	parentCommitOid string
+	author          string
+	time            int64
+	message         string
+}
+
 func GenInfo(objectType string, fileContent []byte) (*ObjectInfo, error) {
 	if objectType != BLOB_OBJECT_TYPE &&
 		objectType != TAG_OBJECT_TYPE &&
@@ -102,6 +110,20 @@ func Read(oid string) (*ObjectInfo, error) {
 	}
 
 	splitted := bytes.Split(fileContent, []byte("\x00"))
+
+	// commit object file
+	if len(splitted) == 1 {
+		commitContent := splitted[0]
+		return &ObjectInfo{
+			Type:       COMMIT_OBJECT_TYPE,
+			Size:       len(commitContent),
+			Content:    commitContent,
+			RawContent: commitContent,
+			Oid:        oid,
+		}, nil
+	}
+
+	// blob or tree object file
 	header, content := splitted[0], splitted[1]
 
 	headerSplitted := bytes.Split(header, []byte(" "))
@@ -225,18 +247,16 @@ func Commit(msg string) (string, error) {
 	// parent commit
 	// @todo: commit the same working directory will cause parent to point to the same commit
 	// How to check no changes since last commit?
-	refPointed, err := refs.GetCurrentHead()
+	headRef, err := refs.GetCurrentHead()
 	if err != nil {
 		return "", fmt.Errorf("failed to read HEAD file, %v", err)
 	}
 
-	refContent, err := refs.GetRefContent(refPointed)
+	prevCommitOid, err := refs.GetRefContent(headRef)
 	if err != nil {
 		return "", fmt.Errorf("failed to read ref file, %v", err)
 	}
-	if refContent != "" {
-		fileContent = append(fileContent, []byte(fmt.Sprintf("parent %v\n", refContent))...)
-	}
+	fileContent = append(fileContent, []byte(fmt.Sprintf("parent %v\n", prevCommitOid))...)
 
 	// author
 	usr, err := user.Current()
@@ -257,9 +277,51 @@ func Commit(msg string) (string, error) {
 	}
 
 	// @todo: How to handle if error happened here?
-	refs.SetRefContent(refPointed, commitOid)
+	refs.SetRefContent(headRef, commitOid)
 
 	return commitOid, nil
+}
+
+func PrintCommitLogs() error {
+	headRef, err := refs.GetCurrentHead()
+	if err != nil {
+		return fmt.Errorf("failed to read HEAD file, %v", err)
+	}
+
+	commitOid, err := refs.GetRefContent(headRef)
+	if err != nil {
+		return fmt.Errorf("failed to read ref file, %v", err)
+	}
+	if commitOid == "" {
+		return fmt.Errorf("fatal: Your branch does not have any commits yet")
+	}
+
+	commitInfo, err := parseCommitContent(commitOid)
+	if err != nil {
+		return err
+	}
+
+	/*
+		commit: <oid>
+		Author: author
+		Date: <date>
+
+		    commit message
+	*/
+
+	t := time.Unix(commitInfo.time, 0)
+	fmt.Printf("commit: %v\nAuthor: %v\nDate: %v\n\n\t%v\n\n", commitOid, commitInfo.author, t.String(), commitInfo.message)
+
+	for curr := commitInfo; curr.parentCommitOid != ""; {
+		curr, err = parseCommitContent(curr.parentCommitOid)
+		if err != nil {
+			return err
+		}
+		t := time.Unix(commitInfo.time, 0)
+		fmt.Printf("commit: %v\nAuthor: %v\nDate: %v\n\n\t%v\n\n", commitOid, curr.author, t.String(), curr.message)
+	}
+
+	return nil
 }
 
 func recursivelyReadTree(oid, prefix string, treeEntries *[]treeEntry) error {
@@ -301,4 +363,32 @@ func recursivelyReadTree(oid, prefix string, treeEntries *[]treeEntry) error {
 	}
 
 	return nil
+}
+
+func parseCommitContent(commitOid string) (*commitInfo, error) {
+	objInfo, err := Read(commitOid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read commit file, %v", err)
+	}
+	commitContents := strings.Split(string(objInfo.Content), "\n\n")
+	message := commitContents[1]
+
+	infos := strings.Split(commitContents[0], "\n")
+	treeOid := strings.Split(infos[0], " ")[1]
+	parent := strings.Split(infos[1], " ")[1]
+
+	authorAndTime := strings.Split(infos[2], " ")
+	author := authorAndTime[1]
+	timestampInt64, err := strconv.ParseInt(authorAndTime[2], 10, 64)
+	if err != nil {
+		timestampInt64 = 0
+	}
+
+	return &commitInfo{
+		treeOid:         treeOid,
+		parentCommitOid: parent,
+		author:          author,
+		time:            timestampInt64,
+		message:         message,
+	}, nil
 }
--- refs/refs.go
+++ refs/refs.go
@@ -15,6 +15,10 @@ const (
 )
 
 func GetCurrentHead() (string, error) {
+	/*
+		returns the content of HEAD file.
+		e.g: refs/heads/master
+	*/
 	content, err := os.ReadFile(filepath.Join(root.FOLDER_NAME, "HEAD"))
 	if err != nil {
 		return "", err
@@ -29,6 +33,10 @@ func GetCurrentHead() (string, error) {
 }
 
 func GetRefContent(refPath string) (string, error) {
+	/*
+		reads the content of ref pointed by HEAD (an OID to tree object file)
+		and returns it.
+	*/
 	commitOid, err := os.ReadFile(filepath.Join(root.FOLDER_NAME, refPath))
 	if err != nil {
 		return "", err
@@ -38,6 +46,9 @@ func GetRefContent(refPath string) (string, error) {
 }
 
 func SetRefContent(refPath string, commitOid string) (string, error) {
+	/*
+		set the value of a ref to commitOid provided.
+	*/
 	err := os.WriteFile(filepath.Join(root.FOLDER_NAME, refPath), []byte(commitOid), 0o664)
 	if err != nil {
 		return "", err
